761db719ba216d24fc114fb93b926000242dc1fc
Martin Bergesio Trabajo <bergesio90@gmai.com>
2022-01-03 12:43:19 +0100

    React: 95. REDUX Estructura de archivos

--- 07-redux/src/App.js
+++ 07-redux/src/App.js
@@ -1,12 +1,16 @@
 import TeoriaRedux from "./components/TeoriaRedux";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Redux</h1>
-      <hr />
-      <TeoriaRedux />
-    </div>
+    <Provider store={store}>
+      <div style={{ textAlign: "center" }}>
+        <h1>Redux</h1>
+        <hr />
+        <TeoriaRedux />
+      </div>
+    </Provider>
   );
 }
 
