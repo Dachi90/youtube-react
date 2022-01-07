3b67b3cd99833457516f85287fc011942a35aac3
Martin Bergesio Trabajo <bergesio90@gmai.com>
2022-01-07 13:23:16 +0100

    98. REDUX Carrito de Compras ( 2 / 2 )

--- 07-redux/src/App.js
+++ 07-redux/src/App.js
@@ -2,12 +2,15 @@ import TeoriaRedux from "./components/TeoriaRedux";
 import { Provider } from "react-redux";
 import store from "./store";
 import Contador from "./components/Contador";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
     <Provider store={store}>
       <div style={{ textAlign: "center" }}>
         <h1>Redux</h1>
+        <ShoppingCart />
+        <hr />
         <Contador />
         <hr />
         <TeoriaRedux />
--- 07-redux/src/components/ShoppingCart.js
+++ 07-redux/src/components/ShoppingCart.js
@@ -1,34 +1,13 @@
-import { useReducer } from "react";
-import { TYPES } from "../actions/shoppingAction";
-import {
-  shoppingInitialState,
-  shoppingReducer,
-} from "../reducers/shoppingReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, clearCart, delFromCart } from "../actions/shoppingAction";
 import CartItem from "./CartItem";
 import ProductItem from "./ProductItem";
 
 const ShoppingCart = () => {
-  const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-  const { products, cart } = state;
-
-  const addToCart = (id) => {
-    //console.log(id);
-    dispatch({ type: TYPES.ADD_TO_CART, payload: id });
-  };
-
-  const delFromCart = (id, all = false) => {
-    //console.log(id, all);
-    if (all) {
-      dispatch({ type: TYPES.REMOVE_ALL_FROM_CART, payload: id });
-    } else {
-      dispatch({ type: TYPES.REMOVE_ONE_FROM_CART, payload: id });
-    }
-  };
-
-  const clearCart = () => {
-    dispatch({ type: TYPES.CLEAR_CART });
-  };
+  const { products, cart } = state.shopping;
 
   return (
     <div>
@@ -36,14 +15,14 @@ const ShoppingCart = () => {
       <h3>Productos</h3>
       <article className="box grid-responsive">
         {products.map((product) => (
-          <ProductItem key={product.id} data={product} addToCart={addToCart} />
+          <ProductItem key={product.id} data={product} addToCart={() => dispatch(addToCart(product.id))} />
         ))}
       </article>
       <h3>Carrito</h3>
       <article className="box">
-        <button onClick={clearCart}>Limpiar Carrito</button>
+        <button onClick={() => dispatch(clearCart())}>Limpiar Carrito</button>
         {cart.map((item, index) => (
-          <CartItem key={index} data={item} delFromCart={delFromCart} />
+          <CartItem key={index} data={item} delOneFromCart={() => dispatch(delFromCart(item.id))} delAllFromCart={(() => dispatch(delFromCart(item.id)), true)} />
         ))}
       </article>
     </div>
